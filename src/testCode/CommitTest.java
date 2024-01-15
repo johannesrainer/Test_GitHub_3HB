46ac762a6c6d399e03edad2666954302b8731cdf
johannesrainer <johannes.rainer@hak-reutte.ac.at>
2024-01-15 10:22:48 +0100

    output message for local commit

--- src/testCode/CommitTest.java
+++ src/testCode/CommitTest.java
@@ -5,6 +5,7 @@ public class CommitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Commit Message Test");
+		System.out.println("local commit test");
 
 	}
 
