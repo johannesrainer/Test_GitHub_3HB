8f2dbc6c47bb74dd506042b5241d33b4c922e5a9
johannesrainer <johannes.rainer@hak-reutte.ac.at>
2024-04-28 22:00:07 +0200

    HÜ - bis 29.04.2023 - Aufgabe 1 und 2 zu String und String Builder

--- src/String_StringBuilder/CheckPassword.java
+++ src/String_StringBuilder/CheckPassword.java
@@ -1,19 +1,45 @@
+
 package String_StringBuilder;
-
+ 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+ 
 public class CheckPassword {
-	
-	public static boolean isGoodPassword(String input) {
-		Pattern characters = Pattern.compile("[a-zA-Z]");
-		Pattern numbers = Pattern.compile("[\\d+]");
-		Pattern specialSigns = Pattern.compile("[!#$%&'()*+,-./:;<=>?@\\]^_`{|}~]");
-		
+ 
+	public static boolean isGoodPassword(String password) {
+		Pattern capitalLetters = Pattern.compile(".*[A-Z].*");
+		Pattern numbers = Pattern.compile("\\d+");
+		Pattern special = Pattern.compile("[!§$%?#*]");
+		// Statt drei Variablen -> Array
+		boolean [] isSafe = new boolean[3];
+		// Ein gutes Passwort enthält:
+		// min. 8 Buchstaben,
+		if (password.length() < 8) {
+			return false;
+		}
+		// ...davon min. 1 Großbuchstabe
+		// Zahlen und Sonderzeichen
+		for (String s : password.split("")) {
+			Matcher cLMatcher = capitalLetters.matcher(s);
+			Matcher numberMatcher = numbers.matcher(s);
+			Matcher specialMatcher = special.matcher(s);
+			if (cLMatcher.matches()) {
+				isSafe[0] = true;
+			} else if (numberMatcher.matches()) {
+				isSafe[1] = true;
+			} else if (specialMatcher.matches()) {
+				isSafe[2] = true;
+			}
+		}
+		if (isSafe[0] && isSafe[1] && isSafe[2]) {
+			return true;
+		}
+		return false;
 	}
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(isGoodPassword("IstDiesGut2?!"));
+		System.out.println(isGoodPassword("UndDasHier"));
+		System.out.println(isGoodPassword("WasHälstDuVonDem123#"));
 	}
-
+ 
 }
